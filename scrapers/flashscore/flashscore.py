2f035c22b48691f1797c8d1b1a3d5885290f68c5
petar.n <petar.n@delasport.com>
2018-04-03 17:23:05 +0300

    push push push

--- scrapers/flashscore/flashscore.py
+++ scrapers/flashscore/flashscore.py
@@ -1,14 +1,19 @@
-import requests, json
+import requests, json, re
 from bs4 import BeautifulSoup as soup
 
 def request():
-    url = "https://www.sofascore.com/football/livescore/json"
+    url = "http://www.flashscore.mobi/"
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
     req = requests.get(url, headers=headers)
-    events = json.loads(req.content)['sportItem']
+    html_content = soup(req.content, "html.parser")
+    events_container = html_content.find("div", {"id":"score-data"})
 
-    print "Content"
-    print events['tournaments'][0]
-    print "Content"
+
+    print "First league"
+    print events_container.find_all('h4')
+    events_container.h4.decompose()
+    event_names = re.compile("\d:\d").split(events_container.get_text())
+    print event_names
+    print "First league"
 
 request()
