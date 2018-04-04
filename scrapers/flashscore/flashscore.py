fa44322144293d766a294677e5155df5922fac3a
petar.n <petar.n@delasport.com>
2018-04-04 17:37:00 +0300

    push push push

--- scrapers/flashscore/flashscore.py
+++ scrapers/flashscore/flashscore.py
@@ -1,19 +1,65 @@
-import requests, json, re
+__author__ = "Petar Netev"
+
+import requests, json, re, sys, traceback
 from bs4 import BeautifulSoup as soup
 
-def request():
-    url = "http://www.flashscore.mobi/"
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
-    req = requests.get(url, headers=headers)
-    html_content = soup(req.content, "html.parser")
-    events_container = html_content.find("div", {"id":"score-data"})
+
+URL_SPORTS_MAP = {
+    'football': '',
+    'hockey': 'hockey',
+    'tennis': 'tennis',
+    'basketball': 'basketball',
+    'handball': 'handball',
+}
+
+URL_PERIODS_MAP = {
+    'all': [('today',''),('yesterday','?d=-1'),('tomorrow','?d=1')],
+    'live': '?s=2',
+    'finished': '?s=3'
+}
+
+class FlashScore:
+    def __init__(self, period, sport, sleeptime):
+        self.host = 'http://www.flashscore.mobi'
+        self.period = period
+        self.sport = sport
+        self.sleeptime = sleeptime
+        self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36'}
+        self.session = requests.Session()
+        self.session.headers = self.headers
+
+    def get_data(self, period, sport):
+        try:
+            if period != 'all':
+                url = "{}/{}/{}".format(self.host, URL_SPORTS_MAP[sport],URL_PERIODS_MAP[period])
+                req = self.session.get(url)
+                events_container = soup(req.content, "html.parser").find("div",{"id":"score-data"})
+                return events_container
+            else:
+                three_days_events = []
+                for day in URL_PERIODS_MAP[period]:
+                    url = "{}/{}/{}".format(self.host, URL_SPORTS_MAP[sport], day[1])
+                    req = self.session.get(url)
+                    three_days_events.append(soup(req.content, "html.parser").find("div",{"id":"score-data"}))
+                return three_days_events
+        except:
+            print "There is some problem with the data container get"
+            traceback.print_exc()
+
+    def scrape_football(self, period):
+        events = {}
+
+        events_data = self.get_data(period,self.sport)
+        tournaments = events_data.find_all('h4')
+        for tournament in tournaments:
+            if tournament.text not in events:
+                events[tournament.text] = []
+
+        print events
 
 
-    print "First league"
-    print events_container.find_all('h4')
-    events_container.h4.decompose()
-    event_names = re.compile("\d:\d").split(events_container.get_text())
-    print event_names
-    print "First league"
+    def start(self):
+        self.scrape_football(self.period)
 
-request()
+f = FlashScore('live','tennis',20)
+f.start()
